760a1dff7738e9210f49c7c95f4133522b4f9973
sotam1069 <pedro.matos9@upr.edu>
2022-05-30 02:55:28 -0400

    Same functionality but complete rework of codebase

--- TicTacToe/src/TicTacToeGame.java
+++ TicTacToe/src/TicTacToeGame.java
@@ -1,143 +1,169 @@
+import java.sql.Array;
 import java.util.*;
 
-public class TicTacToeGame{
+public class TicTacToeGame {
+
+
+    public static final int X = 1, O = -1;
+    public static final int EMPTY  = 0;
+
+    private static Integer[][] board = new Integer[3][3];
+    private int playerTurn = 0;
+    private static String player = "player";
+    private static String cpu = "cpu";
+
     static ArrayList<Integer> playerPositions = new ArrayList<Integer>();
     static ArrayList<Integer> cpuPositions = new ArrayList<Integer>();
 
+
+    public TicTacToeGame() { clearBoard(); }
+
+    public void clearBoard(){
+        ;
+        for(int x=0; x < 3; x++){
+            for(int y=0; y < 3; y++){
+                board[x][y] = EMPTY;
+            }
+        }
+        playerTurn = X;
+    }
+
+    public static void putMark(int pos, String user){
+
+        int symbol = EMPTY;
+
+        if(user.equalsIgnoreCase(player)){
+            symbol = X;
+            playerPositions.add(pos);
+        }
+        else if (user.equalsIgnoreCase(cpu)){
+            symbol = O;
+            cpuPositions.add(pos);
+        }
+
+        switch(pos){
+
+            case 1:
+                board[0][0] = symbol;
+                break;
+            case 2:
+                board[0][1] = symbol;
+                break;
+            case 3:
+                board[0][2] = symbol;
+                break;
+            case 4:
+                board[1][0] = symbol;
+                break;
+            case 5:
+                board[1][1] = symbol;
+                break;
+            case 6:
+                board[1][2] = symbol;
+                break;
+            case 7:
+                board[2][0] = symbol;
+                break;
+            case 8:
+                board[2][1] = symbol;
+                break;
+            case 9:
+                board[2][2] = symbol;
+                break;
+            default:
+                break;
+        }
+    }
+
+    public static boolean isWin(int mark){
+
+
+        return ((board[0][0] + board[0][1] + board[0][2] == mark*3)
+                || (board[1][0] + board[1][1] + board[1][2] == mark*3)
+                || (board[2][0] + board[2][1] + board[2][2] == mark*3)
+                || (board[0][0] + board[1][0] + board[2][0] == mark*3)
+                || (board[0][1] + board[1][1] + board[2][1] == mark*3)
+                || (board[0][2] + board[1][2] + board[2][2] == mark*3)
+                || (board[0][0] + board[1][1] + board[2][2] == mark*3)
+                || (board[2][0] + board[1][1] + board[0][2] == mark*3));
+    }
+
+    public static int winner(){
+        if (isWin(X)){
+            return(X);
+        }
+        else if(isWin(O)){
+            return(O);
+        }
+        else{
+            return 0;
+        }
+    }
+
+    public String toString(){
+
+        StringBuilder sb = new StringBuilder();
+
+
+        for(int x=0; x < 3; x++){
+            for(int y=0; y < 3; y++){
+                switch(board[x][y]){
+                    case X:
+                        sb.append("X");
+                        break;
+                    case O:
+                        sb.append("O");
+                        break;
+                    case EMPTY:
+                        sb.append(" ");
+                        break;
+                }
+
+                if (y < 2) { sb.append("|"); }
+
+            }
+            if(x < 2) { sb.append("\n----- \n"); }
+        }
+        return sb.toString();
+    }
+
     public static void main(String[] args){
-        // Create gameBoard
-        char[][] gameBoard = {{' ', '|',' ','|',' '},
-                              {'-', '+','-','+','-'},
-                              {' ', '|',' ','|',' '},
-                              {'-', '+','-','+','-'},
-                              {' ', '|',' ','|',' '}};
 
-        printGameBoard(gameBoard);
+        TicTacToeGame game = new TicTacToeGame();
+        System.out.println(game);
 
-        //Input statements and checks for remaining positions
-
-        while (true) {
+        while(true){
             Scanner scan = new Scanner(System.in);
-            System.out.println("Print your placement (1-9): ");
+            System.out.println("Print your placement on board (1-9): ");
             int playerPos = scan.nextInt();
-
+            while(playerPos <= 0 || playerPos > 9){
+                System.out.println("Incorrect position please try again: ");
+                playerPos = scan.nextInt();
+            }
             while(playerPositions.contains(playerPos) || cpuPositions.contains(playerPos)){
                 System.out.println("Position taken, Enter a correct position again");
                 playerPos = scan.nextInt();
             }
+            putMark(playerPos,player);
 
-            placePiece(gameBoard,playerPos,"player");
-
-            String result = checkWinner();
+            int result = winner();
 
             Random rand = new Random();
             int cpuPos = rand.nextInt(9) + 1;
             while(playerPositions.contains(cpuPos) || cpuPositions.contains(cpuPos)){
                 cpuPos = rand.nextInt(9) + 1;
             }
-            placePiece(gameBoard,cpuPos,"cpu");
+            putMark(cpuPos,cpu);
+            System.out.println(game);
 
-            printGameBoard(gameBoard);
-
-            result = checkWinner();
-            if(result.length() > 0){
-                System.out.println(result);
+            result = winner();
+            String[]outcome={"Cpu wins the game!","It's a tie!","You win the game!"};
+            if(result == X || result == O){
+                System.out.println(outcome[1 + result]);
                 break;
             }
+
         }
     }
-    //Checks the winner using the position of the player/cpu and compares it to the Lists
-    public static String checkWinner(){
 
-        List topRow = Arrays.asList(1,2,3);
-        List midRow = Arrays.asList(4,5,6);
-        List botRow = Arrays.asList(7,8,9);
-        List leftCollumn = Arrays.asList(1,4,7);
-        List rightCollumn = Arrays.asList(3,6,9);
-        List midCollumn = Arrays.asList(2,5,8);
-        List cross1 = Arrays.asList(1,5,9);
-        List cross2 = Arrays.asList(7,5,3);
-
-
-        List<List> winningConditions = new ArrayList<List>();
-        winningConditions.add(topRow);
-        winningConditions.add(midRow);
-        winningConditions.add(botRow);
-        winningConditions.add(leftCollumn);
-        winningConditions.add(rightCollumn);
-        winningConditions.add(midCollumn);
-        winningConditions.add(cross1);
-        winningConditions.add(cross2);
-
-        //Checks winning conditions
-
-        for(List l : winningConditions){
-            if(playerPositions.containsAll(l)){
-                return "Congratulations you have won the game!";
-            }else if(cpuPositions.containsAll(l)){
-                return "CPU Wins! Sorry :(";
-            } else if(playerPositions.size() + cpuPositions.size() == 9){
-                return "CAT!";
-            }
-        }
-
-        return "";
-    }
-
-    public static void printGameBoard(char[][] gameBoard){
-
-        for(char[] row: gameBoard){
-            for(char c: row){
-                System.out.print(c);
-            }
-            System.out.println();
-        }
-    }
-    public static void placePiece(char[][] gameBoard,int pos, String user){
-
-        char symbol = ' ';
-
-        if(user.equalsIgnoreCase("player")){
-            symbol = 'X';
-            playerPositions.add(pos);
-        }else if (user.equalsIgnoreCase("cpu")){
-            symbol = '0';
-            cpuPositions.add(pos);
-        }
-
-        //Adds X or O to the position in the board
-
-        switch(pos){
-
-            case 1:
-                gameBoard[0][0] = symbol;
-                break;
-            case 2:
-                gameBoard[0][2] = symbol;
-                break;
-            case 3:
-                gameBoard[0][4] = symbol;
-                break;
-            case 4:
-                gameBoard[2][0] = symbol;
-                break;
-            case 5:
-                gameBoard[2][2] = symbol;
-                break;
-            case 6:
-                gameBoard[2][4] = symbol;
-                break;
-            case 7:
-                gameBoard[4][0] = symbol;
-                break;
-            case 8:
-                gameBoard[4][2] = symbol;
-                break;
-            case 9:
-                gameBoard[4][4] = symbol;
-                break;
-            default:
-                break;
-        }
-    }
 }
